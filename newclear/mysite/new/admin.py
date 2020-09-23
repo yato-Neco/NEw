a243bd273fc23b2eb72a7fb40bd54e659a8e1c58
x64Neco <x64Neco@gmail.com>
2020-09-24 00:59:49 +0900

    New Project.
    web service

---
+++ newclear/mysite/new/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from new.models import Test
+
+admin.site.register(Test)
+# Register your models here.
---
+++ newclear/mysite/new/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Test(models.Model):
+    name = models.CharField('書籍名', max_length=255)
+    publisher = models.CharField('出版社', max_length=255, blank=True)
+    page = models.IntegerField('ページ数', blank=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
